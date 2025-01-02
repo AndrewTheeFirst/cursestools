9b15cc7039a11643ed36704c31950b53c92f1862
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2025-01-02 18:51:53 -0500

    Updated to reflect Terminal Alignment and Textbox additions.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-from .widgets import Panel, FreeWindow, TextBox, Page
+from .widgets import Panel, FreeWindow, Terminal, Page, TextBox
 from .utils import \
     draw_box, draw_button,\
         center, wprint, wread,\
             cover, uncover,\
                 reprint_win, lay
-from .consts import Dir,\
+from .consts import Dir, Align,\
     ENTER, BKSP, CTRL_BKSP, ESC
